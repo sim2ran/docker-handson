c5fb5559f7b63dbbe2d4515d6b4b5cda96fb9173
sim2ran <123307204+sim2ran@users.noreply.github.com>
2025-01-26 08:48:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+// MongoDB connection
+mongoose
+  .connect("mongodb://root:rootpassword@mongodb:27017/testdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello from the Node.js backend!");
+});
+
+app.listen(port, () => {
+  console.log(`Backend app is running on http://localhost:${port}`);
+});
